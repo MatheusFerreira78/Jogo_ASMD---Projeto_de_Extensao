3f07e72ba8947ee553d10bb0f277445fccdf91d1
Matheus Vinicius Ferreira e Silva <matheus10.silva@alunos.ifsuldeminas.edu.br>
2023-09-23 12:13:07 -0300

    alteração no autopreenchimento no css e teste js da tela

---
+++ Tela_Inicial/tela.js
@@ -0,0 +1,23 @@
+/*const botao_enivar = document.querySelector("#enviar");
+
+botao_enivar.addEventListener("click", function(){
+    
+    const jogador_1 = document.querySelector("#nome1");
+    const jogador_2 = document.querySelector("#nome2");
+    const jogador_3 = document.querySelector("#nome3");
+    const jogador_4 = document.querySelector("#nome4");
+    const jogador_5 = document.querySelector("#nome5");
+
+    const nome1 = jogador_1.value;
+    const nome2 = jogador_2.value;
+    const nome3 = jogador_3.value;
+    const nome4 = jogador_4.value;
+    const nome5 = jogador_5.value;
+
+    console.log(nome1);
+    console.log(nome2);
+    console.log(nome3);
+    console.log(nome4);
+    console.log(nome5);
+    
+});*/
