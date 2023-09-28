ed3e08ad2a095f06d02be14bc99346b329b935b1
Matheus Vinícius Ferreira e Silva <202111020038@pcs.ifsuldeminas.local>
2023-09-28 11:35:23 -0300

    ATT

--- Jogo/Jogo.js
+++ Jogo/Jogo.js
@@ -1,6 +1,5 @@
 function Dado_aleatorio() {
     const random = Math.floor(Math.random() * 6) + 1;
-    console.log(random);
 
     return random;
 }
@@ -14,12 +13,11 @@ function Operacao_aleatoria() {
 
   const randomIndex = Math.floor(Math.random() * 4);
   const operacao = operations[randomIndex];
-  console.log(operacao);
 
   return operacao;
 
 }
-
+/*
 function Rodar_operacao_1() {
 
   var random = Operacao_aleatoria();
@@ -103,7 +101,7 @@ function Rodar_dado_2() {
     document.getElementById("dado_2").innerHTML = "<img src='face6.png'/>";
   }
 
-}
+} 
 
 function Rodar_dado_3() {
   
@@ -127,15 +125,166 @@ function Rodar_dado_3() {
   else if (random == 6) {
     document.getElementById("dado_3").innerHTML = "<img src='face6.png'/>";
   }
+} */
+
+var Resposta_geral = 0;
+
+function calcular() {
+  
+    var dado_1 = Dado_aleatorio();
+    var dado_2 = Dado_aleatorio();
+    var dado_3 = Dado_aleatorio();
+    var operacao_1 = Operacao_aleatoria();
+    var operacao_2 = Operacao_aleatoria();
+    var resultado = 0;
+    var resultado_2 = 0;
+  
+    if (operacao_1 == 1) {
+      resultado = dado_1 + dado_2;
+    }
+    else if (operacao_1 == 2) {
+      resultado = dado_1 - dado_2;
+    }
+    else if (operacao_1 == 3) {
+      resultado = dado_1 * dado_2;
+    }
+    else if (operacao_1 == 4) {
+      resultado = dado_1 / dado_2;
+    }
+
+    if (operacao_2 == 1) {
+      resultado_2 = resultado + dado_3;
+    }
+    else if (operacao_2 == 2) {
+      resultado_2 = resultado - dado_3;
+    }
+    else if (operacao_2 == 3) {
+      resultado_2 = resultado * dado_3;
+    }
+    else if (operacao_2 == 4) {
+      resultado_2 = resultado / dado_3;
+    }
+
+    if (dado_1 == 1) {
+      document.getElementById("dado_1").innerHTML = "<img src='face1.png'/>";
+    }
+    else if (dado_1 == 2) {
+      document.getElementById("dado_1").innerHTML = "<img src='face2.png'/>";
+    }
+    else if (dado_1 == 3) {
+      document.getElementById("dado_1").innerHTML = "<img src='face3.png'/>";
+    }
+    else if (dado_1 == 4) {
+      document.getElementById("dado_1").innerHTML = "<img src='face4.png'/>";
+    }
+    else if (dado_1 == 5) {
+      document.getElementById("dado_1").innerHTML = "<img src='face5.png'/>";
+    }
+    else if (dado_1 == 6) {
+      document.getElementById("dado_1").innerHTML = "<img src='face6.png'/>";
+    }
+
+    if (dado_2 == 1) {
+      document.getElementById("dado_2").innerHTML = "<img src='face1.png'/>";
+    }
+    else if (dado_2 == 2) {
+      document.getElementById("dado_2").innerHTML = "<img src='face2.png'/>";
+    }
+    else if (dado_2 == 3) {
+      document.getElementById("dado_2").innerHTML = "<img src='face3.png'/>";
+    }
+    else if (dado_2 == 4) {
+      document.getElementById("dado_2").innerHTML = "<img src='face4.png'/>";
+    }
+    else if (dado_2 == 5) {
+      document.getElementById("dado_2").innerHTML = "<img src='face5.png'/>";
+    }
+    else if (dado_2 == 6) {
+      document.getElementById("dado_2").innerHTML = "<img src='face6.png'/>";
+    }
+
+    if (dado_3 == 1) {
+      document.getElementById("dado_3").innerHTML = "<img src='face1.png'/>";
+    }
+    else if (dado_3 == 2) {
+      document.getElementById("dado_3").innerHTML = "<img src='face2.png'/>";
+    }
+    else if (dado_3 == 3) {
+      document.getElementById("dado_3").innerHTML = "<img src='face3.png'/>";
+    }
+    else if (dado_3 == 4) {
+      document.getElementById("dado_3").innerHTML = "<img src='face4.png'/>";
+    }
+    else if (dado_3 == 5) {
+      document.getElementById("dado_3").innerHTML = "<img src='face5.png'/>";
+    }
+    else if (dado_3 == 6) {
+      document.getElementById("dado_3").innerHTML = "<img src='face6.png'/>";
+    }
+
+    if (operacao_1 == 1) {
+      document.getElementById("operacao_1").innerHTML = "<img src='ponteiro_soma.png'/>";
+    }
+    else if (operacao_1 == 2) {
+      document.getElementById("operacao_1").innerHTML = "<img src='ponteiro_subtracao.png'/>";
+    }
+    else if (operacao_1 == 3) {
+      document.getElementById("operacao_1").innerHTML = "<img src='ponteiro_multiplicacao.png'/>";
+    }
+    else if (operacao_1 == 4) {
+      document.getElementById("operacao_1").innerHTML = "<img src='ponteiro_divisao.png'/>";
+    }
+    
+    if (operacao_2 == 1) {
+      document.getElementById("operacao_2").innerHTML = "<img src='ponteiro_soma.png'/>";
+    }
+    else if (operacao_2 == 2) {
+      document.getElementById("operacao_2").innerHTML = "<img src='ponteiro_subtracao.png'/>";
+    }
+    else if (operacao_2 == 3) {
+      document.getElementById("operacao_2").innerHTML = "<img src='ponteiro_multiplicacao.png'/>";
+    }
+    else if (operacao_2 == 4) {
+      document.getElementById("operacao_2").innerHTML = "<img src='ponteiro_divisao.png'/>";
+    }
+
+    console.log(dado_1);
+    console.log(operacao_1);
+    console.log(dado_2);
+    console.log(resultado);
+    console.log(operacao_2);
+    console.log(dado_3);
+    console.log(resultado_2);
+    
+    Resposta_geral = resultado_2;
+}
+
+function verifcar_resposta() {
+
+  const botao = document.querySelector("#botao_resposta");
+
+  botao.addEventListener("click", function(e) {
+
+    e.preventDefault();
+
+    var resposta = document.querySelector("#resposta");
+
+    var valor = resposta.value;
+    console.log(valor);
+  });
+
+  if (valor == Resposta_geral) {
+    console.log("Acertou");
+  } else { 
+    console.log("Errou");
+  }
+
 }
 
 function main(){
 
-  Rodar_dado_1();
-  Rodar_dado_2();
-  Rodar_dado_3();
-  Rodar_operacao_1();
-  Rodar_operacao_2();
+  calcular();
+  verifcar_resposta();
   
 }
 
